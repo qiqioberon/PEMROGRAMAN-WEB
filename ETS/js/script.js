c5aebb9b31b846a261103758b0c9e2fa95c25ec7
qiqioberon <117354912+qiqioberon@users.noreply.github.com>
2023-10-16 09:48:32 +0700

    Update script.js

--- ETS/js/script.js
+++ ETS/js/script.js
@@ -1,20 +1,31 @@
 $.ajax({
-    url: 'http://api.apilayer.com/live', 
-    dataType: 'json',
+    url: 'http://apilayer.net/api/live',
     type: 'get',
+    dataType: 'json',
     data: {
-      'access_key': 'a5398be57483c66ef6161ae5cf1f07e3',
-      'format': 1,
-      'source': 'EUR',
+        'access_key':'a5398be57483c66ef6161ae5cf1f07e3',
+        'format':1
     },
-    success: function(result) {
-      console.log(result);
-      if (result.success === true) {
-        
-      }
-    },
-    error: function(xhr, status, error) {
-      console.log('Terjadi kesalahan: ' + error);
+    success: function (result) {
+        console.log(result);
+        console.log(result);
+        const rate=result.quotes;
+            $.each(rate, function (i, t) {
+                $('isi').append(
+                `<div class="col-md-3">
+                    <div class="card m-3"> 
+                        <div class="card-body bg-success-subtle">
+                            <div class="row">
+                                <div class="col-2">
+                                ${i}
+                                </div>
+                                <div class="col-9">
+                                ${t}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>);`)
+            });
     }
-  });
-  
+})
