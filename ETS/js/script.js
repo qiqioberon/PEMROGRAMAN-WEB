4df810be3c8edaccf38b954cc1e5d16963598d82
qiqioberon <117354912+qiqioberon@users.noreply.github.com>
2023-10-16 09:17:06 +0700

    Create script.js

---
+++ ETS/js/script.js
@@ -0,0 +1,20 @@
+$.ajax({
+    url: 'http://api.apilayer.com/live', // Perhatikan URL yang digunakan harus benar sesuai dengan API yang Anda tuju
+    dataType: 'json',
+    type: 'get',
+    data: {
+      'access_key': 'a5398be57483c66ef6161ae5cf1f07e3',
+      'format': 1,
+      'source': 'EUR',
+    },
+    success: function(result) {
+      console.log(result);
+      if (result.success === true) {
+        
+      }
+    },
+    error: function(xhr, status, error) {
+      console.log('Terjadi kesalahan: ' + error);
+    }
+  });
+  
